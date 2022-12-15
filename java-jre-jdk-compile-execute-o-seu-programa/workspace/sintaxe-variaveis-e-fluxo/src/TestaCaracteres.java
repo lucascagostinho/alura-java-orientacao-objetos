aa71290ab635834c8dc0fb2bcf86bde6251adaed
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2022-12-14 22:00:16 -0300

    Signed-off-by: Lucas Corrêa Agostinho <lucascagostinho@gmail.com>

---
+++ java-jre-jdk-compile-execute-o-seu-programa/workspace/sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,44 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 65;
+		System.out.println(valor);
+		
+		valor = (char) (valor +1);
+		System.out.println(valor);
+		
+		String palavra = "alura cursos online de tecnologia";
+		
+		System.out.println(palavra);
+		
+		palavra = palavra + 2022;	
+		
+		System.out.println(palavra);
+		
+		String teste1 = "2022";
+		
+		String teste2 = "2021";
+		
+		String teste3 = "2022" + 2022;
+		
+		System.out.println(teste1 + teste2);
+		
+		System.out.println("2022" + "2022");
+		
+		System.out.println(2022 + 2022);
+		
+		System.out.println("2022" + 2022);
+		
+		System.out.println(teste1 + 2022);
+		
+		System.out.println(teste3);
+		
+		
+	}
+
+}
---
+++ java-jre-jdk-compile-execute-o-seu-programa/workspace/sintaxe-variaveis-e-fluxo/src/TestePontoFlutuante.java
@@ -0,0 +1,27 @@
+
+public class TestePontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		
+		System.out.println("O valor do salário é: " + salario);
+		
+		// não irá compilar int valor = 12.5;
+		// não irá compilar int valor2 = 0.0;
+		
+		//irá compilar double teste = 125.50;
+		
+		//não irá compilar int valor3 = teste;
+		
+		int divisao = 5 / 2;
+		
+		System.out.println(divisao);
+		
+		double divisao2 = 5.0 / 2;
+		
+		System.out.println(divisao2);
+		
+	}
+
+}
