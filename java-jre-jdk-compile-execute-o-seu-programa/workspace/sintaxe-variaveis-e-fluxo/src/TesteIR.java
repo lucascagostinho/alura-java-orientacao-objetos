a7521d61a1ede2f42d62457d7743f89dd38be60f
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2023-01-23 20:38:41 -0300

    finalizando pratica de condicionais do curso

--- java-jre-jdk-compile-execute-o-seu-programa/workspace/sintaxe-variaveis-e-fluxo/src/TesteIR.java
+++ java-jre-jdk-compile-execute-o-seu-programa/workspace/sintaxe-variaveis-e-fluxo/src/TesteIR.java
@@ -1,22 +1,22 @@
 
 public class TesteIR {
-	
+
 	public static void main(String[] args) {
-		
+
 		double salario = 3300.0;
-		
-		if (salario >= 1900.0 || salario <=2800.0) {
+
+		if (salario >= 1900.0 && salario <= 2800.0) {
 			salario = salario - 142;
 		} else {
-			if (salario >= 2800.1 || salario <= 3751.0) {
+			if (salario >= 2800.1 && salario <= 3751.0) {
 				salario = salario - 350;
 			} else {
-				if (salario >= 3751.01 || salario <= 4664.00) {
+				if (salario >= 3751.01 && salario <= 4664.00) {
 					salario = salario - 636;
 				}
 			}
 		}
-		
+
 	}
 
 }
