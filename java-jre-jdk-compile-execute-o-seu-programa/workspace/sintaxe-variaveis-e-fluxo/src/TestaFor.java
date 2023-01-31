33f16bdf1e717148a11c759e85c2c6e2e684352d
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2023-01-31 19:38:21 -0300

    Signed-off-by: Lucas Corrêa Agostinho <lucascagostinho@gmail.com>

---
+++ java-jre-jdk-compile-execute-o-seu-programa/workspace/sintaxe-variaveis-e-fluxo/src/TestaFor.java
@@ -0,0 +1,19 @@
+public class TestaFor {
+
+	public static void main(String[] args) {
+		
+		for(int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+		
+		for(int contador = 1; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+		
+		for(int contatdor = 0; contatdor <= 10; contatdor += 1) {
+			System.out.println(contatdor);
+		}
+		
+	}
+	
+}
